f15ee12faea45ccbdb0d189f857d9d57c5a95f7a
fotparask <fotparaskevop@gmail.com>
2021-05-19 16:52:30 +0300

    Timer and file name update

---
+++ studyRoadAndroid/app/src/main/java/com/example/studyroad_android/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.studyroad_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button startButton;
+    private TextView timeRemaining;
+    private ProgressBar timerProgress;
+
+    private CountDownTimer countDownTimer1;
+    private long timeLeft = 600000; //10mins
+    private boolean timerRunning;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timeRemaining = findViewById(R.id.timerText);
+        startButton = findViewById(R.id.timerButton);
+        timerProgress = findViewById(R.id.progressBar);
+
+        startButton.setOnClickListener(v -> timerButtonAction());
+    }
+
+    public void timerButtonAction(){
+        if(timerRunning == false){
+            startTimer();
+        }
+        else{
+            countDownTimer1.cancel();
+            timerRunning = false;
+            startButton.setText("Start");
+        }
+    }
+
+    public void startTimer(){
+        countDownTimer1 = new CountDownTimer(timeLeft, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeLeft = millisUntilFinished;
+                updateUI();
+            }
+
+            @Override
+            public void onFinish(){}
+        }.start();
+
+        startButton.setText("Stop");
+        timerRunning = true;
+    }
+
+    public void updateUI(){
+        long progressPercent = (timeLeft*100)/600000;
+        int minutes = (int) timeLeft / 60000;
+        int seconds = (int) timeLeft % 60000 / 1000;
+        String timeLeftText;
+
+        timeLeftText = "" + minutes;
+        timeLeftText += ":";
+        if(seconds < 10) timeLeftText += 0;
+        timeLeftText += seconds;
+
+        timeRemaining.setText(timeLeftText);
+        timerProgress.setProgress((int) progressPercent);
+    }
+
+}
